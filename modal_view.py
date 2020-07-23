08ecb76bb1a3e3db5028f9ed7d29bcd97beadec0
Pavlos Ioannou <63022538+Pioank@users.noreply.github.com>
2020-07-23 21:46:40 +0100

    Update modal_view.py

--- modal_view.py
+++ modal_view.py
@@ -6,15 +6,15 @@ def modal_view(channel_id): # I have to add channel id here to be posible to sen
         "callback_id": "modal-id",
         "title": {
           "type": "plain_text", #Takes only specific values such as plain_text check Slack API documentation
-          "text": "UNO data request"
+          "text": "Data request" #This is the caption visible on the modal
         },
         "submit": {
           "type": "plain_text", 
-          "text": "Submit"
+          "text": "Submit" #CTA text
         },
         "close": {
           "type": "plain_text",
-          "text": "Cancel"
+          "text": "Cancel" #CTA text
         },
         "blocks": [
           options_field("block_channel_id", "Channel ID", "channel_action", [ { # this field was prepared bacause in request from dialog there is no channel id
@@ -24,9 +24,9 @@ def modal_view(channel_id): # I have to add channel id here to be posible to sen
                   },
                   "value": channel_id
                 }]),
-          input_field( "block_id1", "Provide a marketing ID (Campaign ID, Creative ID etc.)", "a-id"),
-          datepicker_field("block_id2", "Date from", "action_id2"),
-          datepicker_field("block_id3", "Date to", "action_id3"),
+          input_field( "block_id1", "Provide a marketing ID (Campaign ID, Creative ID etc.)", "a-id"), #Def from the fields.py
+          datepicker_field("block_id2", "Date from", "action_id2"),  #Def from the fields.py
+          datepicker_field("block_id3", "Date to", "action_id3"),  #Def from the fields.py
           options_field("block_id4", "Country", "action_id4", [
                 {
                   "text": {
@@ -34,79 +34,24 @@ def modal_view(channel_id): # I have to add channel id here to be posible to sen
                     "text": "United Kindom" # Change to display different
                   },
                   "value": "uk" # Change to pass this value back 
-                },
-                {
-                  "text": {
-                    "type": "plain_text",
-                    "text": "Spain"
-                  },
-                  "value": "es"
-                },
-                {
-                  "text": {
-                    "type": "plain_text",
-                    "text": "Italy"
-                  },
-                  "value": "it"
-                },
-                {
-                  "text": {
-                    "type": "plain_text",
-                    "text": "Rest of the World"
-                  },
-                  "value": "row"
-                }
+                } #You can add more options by extending the same JSON structure
               ]),
           checkboxes_field("block_checkboxes", "Marketing_Channel", "checkboxes_ation", [
                     {
-                        "value": "Affiliates",
+                        "value": "Channel1",
                         "text": {
                             "type": "plain_text",
-                            "text": "Affiliates"
+                            "text": "Channel1"
                         }
                     },
                     {
-                        "value": "Direct",
+                        "value": "Channel2",
                         "text": {
                             "type": "plain_text",
-                            "text": "Direct"
+                            "text": "Channel2"
                         }
                     },
-                    {
-                        "value": "Media",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Media"
-                        }
-                    },
-                    {
-                        "value": "Pay Per Click",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Pay Per Click"
-                        }
-                    },
-                    {
-                        "value": "Programmatic",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Programmatic"
-                        }
-                    },
-                    {
-                        "value": "Search Engine Optimisation",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Search Engine Optimisation"
-                        }
-                    },
-                    {
-                        "value": "Social Media",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Paid Social"
-                        }
-                    },
+
                 ])
         ]
       }
