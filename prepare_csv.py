45d4b27430089c021ab85370e5711182d5c72e40
Pavlos Ioannou <63022538+Pioank@users.noreply.github.com>
2020-07-23 21:53:59 +0100

    Update prepare_csv.py

--- prepare_csv.py
+++ prepare_csv.py
@@ -6,11 +6,13 @@ import numpy as py
 
 def prepare_csv(date_from, date_to, country, markchan, marketingid):
     
-    cnxn = pyodbc.connect("Driver={SQL Server};Server=SC1WNPRNDB003\\DPE_PROD;Database=PresentationLayer;uid=WHGROUP\pkatidis;Trusted_Connection=yes;autocommit=False")
+    cnxn = pyodbc.connect("Driver={SQL Server};Server=;Database=;uid=;Trusted_Connection=yes;autocommit=False")
     cursor = cnxn.cursor()
 
     countryfil = country
     marketingid = str(marketingid)
+    
+    # The lines 16 - 30 are processing the list of marketing channels and returning a string ('channel1', 'n-channel',..,'channel2')that is used in SQL query below
     x=0
     i = int(len(markchan))-1
 
@@ -27,24 +29,19 @@ def prepare_csv(date_from, date_to, country, markchan, marketingid):
             break
         x=x+1
 
-    sql = ("select cab.RegistrationDt, cab.CustomerAccountNumber, cab.username, cou.Countrynm, (case when pla.platformnm <> 'Mobile' then 'Desktop' else 'Mobile' end) as Device, mac.MarketingChannelNm,"
-          " cab.RegistrationTrackingAccountCd, cab.RegistrationTrackingAdCreativeCd, cab.RegistrationTrackingAdSetCd, cab.RegistrationTrackingCampaignCd, cab.RegistrationTrackingCdBtag,"
-          " (case when cab.FirstDepositAmount > 0 THEN 'Y' ELSE 'NO' END) as FD from PresentationLayer.dbo.CustomerAccountBase as cab "
-          "left join PresentationLayer.dbo.ProductSource (NOLOCK) as psc on cab.registrationproductsourceid = psc.productsourceid "
-          "left join PresentationLayer.dbo.MarketingChannel (NOLOCK) as mac on cab.RegistrationMarketingChannelId = mac.MarketingChannelId "
-          "left join PresentationLayer.dbo.country (NOLOCK) as cou on cab.RegistrationCountryId = cou.CountryId "
-          "left join PresentationLayer.dbo.Platform (NOLOCK) as pla on cab.RegistrationPlatformId = pla.PlatformId "
-          "where cab.RegistrationDt > " + "'"+ date_from +"'"
-          "and cab.RegistrationDt < " + "'"+ date_to +"' "
-          "and (CASE WHEN cou.CountryNm = 'United Kingdom' THEN 'uk' WHEN cou.CountryNm = 'Spain' THEN 'es' WHEN cou.CountryNm = 'it' THEN 'IT' ELSE 'row' END) = " + "'"+ countryfil +"' "
-          "and mac.MarketingChannelNm in " + marketingchannelf 
+      
+    sql = ("select FIELDS from PresentationLayer.dbo.CustomerAccountBase as AAA "
+          "left join TABLE (NOLOCK) as XXX on AAA.FIELD1 = XXX.pFIELD1 "
+          "where AAA.date > " + "'"+ date_from +"'"
+          "and AAA.date < " + "'"+ date_to +"' "
+          "and AAA.country = " + "'"+ countryfil +"' "
+          "and XXX.marketingchannel in " + marketingchannelf 
           )
     
     cursor.close()
-    uno = psql.read_sql(sql, cnxn)
+    df = psql.read_sql(sql, cnxn)
 
-    uno['searchkey'] = uno['RegistrationTrackingAccountCd'].map(str) + uno['RegistrationTrackingAdCreativeCd'].map(str) + uno['RegistrationTrackingAdSetCd'].map(str) + uno['RegistrationTrackingCampaignCd'].map(str) + uno['RegistrationTrackingCdBtag'].map(str)
-    uno=uno[uno['searchkey'].str.contains(marketingid)]
-    print(uno)
-
-    return(uno)
+    df['searchkey'] = df['FIELD-A'].map(str) + df['FIELD-B'].map(str) + df['FIELD-C'].map(str)
+    df=df[df['searchkey'].str.contains(marketingid)]
+    
+    return(df)
